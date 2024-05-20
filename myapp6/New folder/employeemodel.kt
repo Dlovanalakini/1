48a044b2e2b1b3d094da73f5795e4af1e8e3c510
Dlovanalakini <dlovan778@gmail.com>
2024-05-20 11:10:28 +0300

    Add files via upload
    added

---
+++ myapp6/New folder/employeemodel.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication
+
+ data  class employeemodel (
+
+    var empid:String?=null,
+    var empname:String?=null,
+    var age:String?=null,
+    var empsalary:String?=null
+)
